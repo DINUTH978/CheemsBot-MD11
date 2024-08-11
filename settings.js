2f72b11f9fa3bb2e0140162c73be694fb79b7347
DINUTH978 <sfsafsdfsdfsfdsfds@gmail.com>
2024-08-11 10:34:19 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,22 +16,22 @@ global.sessionid ='ur session id'
 //owmner v card
 global.ytname = "YT: Xeon" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.location = "Sri Lanka, Galle" //ur location
 
 //new
 global.botname = 'Cheems Bot MD V11' //ur bot name
-global.ownernumber = '916909137213' //ur owner number
-global.ownername = '🦄드림 가이 Xeon' //ur owner name
+global.ownernumber = '94781306128' //ur owner number
+global.ownername = 'DINUTH' //ur owner name
 global.websitex = "https://youtu.be/mu5m6aB6P5k"
 global.wagc = "https://whatsapp.com/channel/0029VaG9VfPKWEKk1rxTQD20"
 global.themeemoji = '🪀'
 global.wm = "Xeon Bot Inc."
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD11' //script link
-global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n+916909137213"
-global.creator = "916909137213@s.whatsapp.net"
+global.botscript = 'https://github.com/DINUTH978/CheemsBot-MD11' //script link
+global.packname = "Sticker By GHOSTx"
+global.author = "🦄드림 가이 Xeon\n\n+94781306128"
+global.creator = "94781306128@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["916909137213"] // Premium User
+global.premium = ["94781306128"] // Premium User
 global.hituet = 0
 
 //bot sett
